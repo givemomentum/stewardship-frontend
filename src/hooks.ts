9096afd149dc0334b234513c0c91852e59ad8432
Victor Yunenko <victor@givemomentum.com>
2022-09-20 14:18:02 -0700

    add useForm hook

---
+++ src/hooks.ts
@@ -0,0 +1,36 @@
+import { FormKitNode } from "@formkit/core";
+import axios from "axios";
+import { ref } from "vue";
+import { strings } from "~/constants";
+
+export function useForm(args: {
+  url: string;
+  dataExtra?: any;
+  serializers?: { [key: string]: (value: any) => any };
+  method?: "POST" | "PATCH";
+}) {
+  const state = {
+    isSuccess: ref(false),
+  };
+  
+  async function submit(data: any, node: FormKitNode) {
+    state.isSuccess.value = false;
+    try {
+      const res = await axios.post(args.url, { ...data, ...args.dataExtra });
+      if (res.data.is_success === true) {
+        state.isSuccess.value = true;
+        node.reset();
+      } else {
+        node.setErrors([strings.error]);
+      }
+    } catch (error) {
+      // todo sentry
+      node.setErrors([strings.error]);
+    }
+  }
+
+  return {
+    submit: submit,
+    isSuccess: state.isSuccess.value,
+  };
+}
---
+++ src/pages/login.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { CContainer, CFlex, CBox } from "@chakra-ui/vue-next";
+import { ref } from "vue";
+import { useForm } from "~/hooks";
+
+const hooks = {
+ form: useForm({
+   url: "/account/login",
+ }),
+};
+
+</script>
+
+<template>
+  <CFlex direction="column">
+    hello world
+  </CFlex>
+</template>
