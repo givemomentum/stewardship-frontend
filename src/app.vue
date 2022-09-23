f11a3990fb6fbff0adae4f68f95a45daf2558368
Victor Yunenko <victor@givemomentum.com>
2022-09-23 13:43:07 -0700

    add navbar & momentum logo

--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
-import { useHead } from "#app";
-import { CContainer, CFlex, CBox } from "@chakra-ui/vue-next";
+  import { useHead } from "#app";
+  import { CContainer, CBox, chakra } from "@chakra-ui/vue-next";
 
-useHead({
-  titleTemplate: (titleChunk) => (titleChunk ? `${titleChunk} - Momentum` : "Momentum"),
-  link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
-});
+  useHead({
+    titleTemplate: (titleChunk) => (titleChunk ? `${titleChunk} - Momentum` : "Momentum"),
+    link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+  });
+
 </script>
 
 <template>
-  <CFlex direction="column" overflowX="hidden">
-    <CContainer max-w="8xl" :px="[3, null, 4]">
+  <CFlex direction="column">
+    <CBox py="4" border-bottom="1px solid" border-color="gray.300">
+      <CContainer max-w="8xl">
+        <chakra.img w="184px" src="momentum-logo.svg" />
+      </CContainer>
+    </CBox>
+
+    <CContainer max-w="8xl" pt="4">
       <NuxtPage />
     </CContainer>
   </CFlex>
