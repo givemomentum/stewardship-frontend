ed298d16b47424a4c5eafb72a2f584c948b1655b
Victor Yunenko <victor@givemomentum.com>
2022-09-23 15:55:03 -0700

    index: add basic tasks styling

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -5,3 +5,29 @@ export interface User {
   first_name: string;
   last_name: string;
 }
+
+export interface Task {
+  pk: PrimaryKey;
+  title: string;
+  description: string;
+  description_short: string;
+  donors: any[];
+  status: TaskStatus;
+  author: User;
+  assignees: User[];
+  assignees_pks: PrimaryKey[];
+  org: PrimaryKey;
+  created_at: string;
+}
+
+export type TaskStatus = 
+  "draft" |
+  "pending" |
+  "recommended" |
+  "recommendation_declined" |
+  "review" |
+  "scheduled" |
+  "in_progress" |
+  "completed" |
+  "failed" |
+  "archived";
