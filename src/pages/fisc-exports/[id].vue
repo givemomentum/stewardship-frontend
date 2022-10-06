3008e25013e6d51938ae5b831ed7ee1d50da2253
Torin Rudeen <torin@givemomentum.com>
2022-10-05 23:24:29 -0700

    Get CSV loading/parsing working

--- src/pages/fisc-exports/[id].vue
+++ src/pages/fisc-exports/[id].vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
   import { useRoute } from "#app";
   import { useApi } from "#imports";
-  import { onBeforeMount, onMounted, ref } from "vue";
+  import { onMounted, ref } from "vue";
   import { FiscExport } from "~/interfaces";
+  import Papa from 'papaparse';
 
   const hooks = {
     route: useRoute(),
@@ -11,17 +12,33 @@
 
   const state = {
     export: ref<FiscExport | null>(null),
+    csvData: ref<any[] | null>(null),
   };
 
-  onBeforeMount(async () => {
+  async function loadExport() {
     const res = await hooks.api.$get(`/fisc/exports/${hooks.route.params.id}`);
     state.export.value = res.data;
+  }
+
+  async function loadCSV() {
+    const csv = await hooks.api.$get(`/fisc/exports/${hooks.route.params.id}/download`)
+    const parseResult = Papa.parse(csv.data, { header: true });
+    if (parseResult.error) {
+      console.log(parseResult.error);
+      // TODO: log to sentry or something
+    }
+    state.csvData.value = parseResult.data;
+  }
+
+  onMounted(() => {
+    loadExport();
+    loadCSV();
   });
 
 </script>
 
 <template>
   <template v-if="state.export.value">
-    <p>Hello, my ID is {{ state.export.value.pk }}</p>
+    <p>{{ state.export.value.pk }}</p>
   </template>
 </template>
