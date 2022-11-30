fc2b0b3f2b9ed88ae6d74f4c1849cba60fae5ec1
Victor <victor.wv2xn@slmail.me>
2022-11-29 17:38:24 -0800

    plugins: add sentry as a plugin

---
+++ src/plugins/sentry.ts
@@ -0,0 +1,32 @@
+import { defineNuxtPlugin } from "#app";
+import { Integrations } from "@sentry/tracing";
+import * as Sentry from "@sentry/vue";
+
+/**
+ * copy of https://github.com/nuxt-community/sentry-module/issues/358#issuecomment-1016983543
+ */
+export default defineNuxtPlugin((nuxtApp) => {
+  if (nuxtApp.$config.public.env !== "dev") {
+    Sentry.init({
+      app: [nuxtApp.vueApp],
+      dsn: "https://4c2d00b85972416ea396bc900e4a8b08@o359384.ingest.sentry.io/6763294",
+      tracesSampleRate: 1.0,
+      environment: nuxtApp.$config.public.env ?? "prod",
+    });
+
+    nuxtApp.vueApp.mixin(
+      Sentry.createTracingMixins({
+        trackComponents: true,
+        timeout: 2000,
+        hooks: ["activate", "mount", "update"],
+      }),
+    );
+    Sentry.attachErrorHandler(nuxtApp.vueApp, {
+      logErrors: false,
+      attachProps: true,
+      trackComponents: true,
+      timeout: 2000,
+      hooks: ["activate", "mount", "update"],
+    });
+  }
+});
