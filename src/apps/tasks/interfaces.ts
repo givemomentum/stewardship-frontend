4f47d3a92781228f0fc3d53d29188ba0ec2c7239
Torin Rudeen <torin@givemomentum.com>
2022-12-04 12:13:57 -0800

    Include is_published field on task

--- src/apps/tasks/interfaces.ts
+++ src/apps/tasks/interfaces.ts
@@ -18,6 +18,7 @@ export interface Task {
   comments: Comment[];
   comments_count: number;
   recommendation_set?: RecommendationSet;
+  is_published: boolean;
 }
 
 export interface Recommendation {
