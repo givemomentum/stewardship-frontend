f942d4743e816ac081d117be49b4fa72519787a8
Victor <36416236+victor-yunenko@users.noreply.github.com>
2023-02-17 17:59:46 -0800

    tiny-mce: try to fix vars loading

--- src/apps/shared/tiny-mce.vue
+++ src/apps/shared/tiny-mce.vue
@@ -2,7 +2,6 @@
   import Editor from "@tinymce/tinymce-vue";
   import { computed, ref, watch } from "vue";
   import { LetterTemplateVariable } from "~/apps/letters/interfaces";
-  import { useStateVar } from "~/structs";
 
   const props = defineProps<{
     modelValue?: string;
@@ -20,82 +19,75 @@
     contentCssDefault?: string;
   }>();
 
-  const comp = {
-    plugins: computed(() => {
-      const plugins = ["anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount pagebreak checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker powerpaste advtable advcode editimage tableofcontents footnotes autocorrect autoresize"];
-      if (props.variables) {
-        plugins.push("mergetags");
-      }
-      return plugins.join(" ");
+  const state = {
+    isVisible: ref(true),
+    vars: ref(composeMergeTags(props.variables)),
+    config: ref({
+      block_formats: "Paragraph=p; Header 1=h1; Header 2=h2; Header 3=h3; Header 4=h4",
+      plugins: "anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount pagebreak checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker powerpaste advtable advcode editimage tableofcontents footnotes autocorrect autoresize",
+      mergetags_list: composeMergeTags(props.variables),
+      toolbar: props.isReadOnly ? "" : "styles | bold italic underline strikethrough | link image mergetags align | numlist bullist indent outdent | removeformat",
+      font_size_formats: "8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt",
+      font_family_formats: "Andale Mono=andale mono,times; Arial=arial, helvetica, sans-serif; Arial Black=arial black, avant garde; Calibri=Calibri; Book Antiqua=book antiqua,palatino; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
+      content_css: "/styles/tiny-mce.css",
+      width: props.width ?? "100%",
+      height: props.minHeight ?? "570px",
+      resize: props.isResizable ?? false,
+      branding: false,
+      menubar: props.isShowMenuBar ?? true,
+      statusbar: false,
+      // can't use css vars in /styles/tiny-mce.css because the editor is an iframe, plus easy to keep those in one place
+      // language=CSS
+      content_style: `
+        :root {
+          --mce-padding: ${props.padding ?? "1in"};
+          --mce-min-height: ${props.minHeight};
+          --mce-editor-box-shadow: ${props.editorBoxShadow};
+          --mce-editor-background: ${props.editorBackground};
+        }
+
+        @media screen {
+          html {
+            background: #f4f4f4;
+            /* noinspection CssUnresolvedCustomProperty */
+            background: var(--mce-editor-background, none);
+            max-height: 100%;
+          }
+        }
+        
+        body {
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+          margin: 0;
+          margin-bottom: 0.5rem;
+          min-height: var(--mce-min-height, 100%);
+          box-shadow: var(--mce-editor-box-shadow, none);
+          padding-left: var(--mce-padding) !important;
+          padding-right: var(--mce-padding) !important;
+        }
+
+        @media screen {
+          body {
+            background-color: #fff;
+            box-sizing: border-box;
+            padding: var(--mce-padding);
+          }
+        }
+        
+        body {
+          ${props.contentCssDefault ?? ""}
+        }
+      `,
     }),
   };
 
-  const state = useStateVar(() => {
-    const isIncludeMergeTags = ref(false);
-    return {
-      isVisible: ref(true),
-      isIncludeMergeTags: isIncludeMergeTags,
-      config: ref({
-        block_formats: "Paragraph=p; Header 1=h1; Header 2=h2; Header 3=h3; Header 4=h4",
-        plugins: comp.plugins.value,
-        toolbar: props.isReadOnly ? "" : "styles | bold italic underline strikethrough | link image mergetags align | numlist bullist indent outdent | removeformat",
-        ...(isIncludeMergeTags.value ? { mergetags_list: composeMergeTags(props.variables) } : {}),
-        font_size_formats: "8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt",
-        font_family_formats: "Andale Mono=andale mono,times; Arial=arial, helvetica, sans-serif; Arial Black=arial black, avant garde; Calibri=Calibri; Book Antiqua=book antiqua,palatino; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats",
-        content_css: "/styles/tiny-mce.css",
-        width: props.width ?? "100%",
-        height: props.minHeight ?? "570px",
-        resize: props.isResizable ?? false,
-        branding: false,
-        menubar: props.isShowMenuBar ?? true,
-        statusbar: false,
-        // can't use css vars in /styles/tiny-mce.css because the editor is an iframe, plus easy to keep those in one place
-        // language=CSS
-        content_style: `
-          :root {
-            --mce-padding: ${props.padding ?? "1in"};
-            --mce-min-height: ${props.minHeight};
-            --mce-editor-box-shadow: ${props.editorBoxShadow};
-            --mce-editor-background: ${props.editorBackground};
-          }
-  
-          @media screen {
-            html {
-              background: #f4f4f4;
-              /* noinspection CssUnresolvedCustomProperty */
-              background: var(--mce-editor-background, none);
-              max-height: 100%;
-            }
-          }
-          
-          body {
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-            margin: 0;
-            margin-bottom: 0.5rem;
-            min-height: var(--mce-min-height, 100%);
-            box-shadow: var(--mce-editor-box-shadow, none);
-            padding-left: var(--mce-padding) !important;
-            padding-right: var(--mce-padding) !important;
-          }
-  
-          @media screen {
-            body {
-              background-color: #fff;
-              box-sizing: border-box;
-              padding: var(--mce-padding);
-            }
-          }
-          
-          body {
-            ${props.contentCssDefault ?? ""}
-          }
-        `,
-      }),
-    };
-  });
-
   watch(() => props.variables, variablesNew => {
-    state.isIncludeMergeTags.value = variablesNew && variablesNew.length > 0;
+    const isIncludeMergeTags = variablesNew && variablesNew.length > 0;
+
+    let plugins = "anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount pagebreak checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker powerpaste advtable advcode editimage tableofcontents footnotes autocorrect autoresize";
+    if (isIncludeMergeTags) {
+      plugins += " mergetags";
+    }
+    state.config.value.plugins = plugins;
   }, { immediate: true });
 
   const emit = defineEmits(["update:modelValue"]);
@@ -109,10 +101,6 @@
     },
   });
 
-  watch(() => props.variables, () => {
-    state.config.value.mergetags_list = composeMergeTags(props.variables);
-  });
-
   function composeMergeTags(vars?: LetterTemplateVariable[]): Array<{ value: string; title: string; }> {
     return vars?.map(variable => ({
       value: variable.key,
@@ -122,14 +110,14 @@
 </script>
 
 <template>
-  <div v-if="state.isVisible.value" class="tiny-mce">
+  <chakra.div v-if="state.isVisible.value" class="tiny-mce">
     <Editor
       api-key="sqzu7ujp1iezh799a6xjbryjl4xjzzcktw0kopn9yxl3mzkr"
       v-model="value"
       :init="state.config.value"
       :disabled="props.isReadOnly ?? false"
     />
-  </div>
+  </chakra.div>
 </template>
 
 <style lang="scss">
