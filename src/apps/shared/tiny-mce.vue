39bdf197ff74588f2622bb896c40cbad78d7fa6f
Victor <36416236+victor-yunenko@users.noreply.github.com>
2023-03-06 17:00:11 -0800

    tiny-mce: fix spellchecker loading

--- src/apps/shared/tiny-mce.vue
+++ src/apps/shared/tiny-mce.vue
@@ -24,7 +24,8 @@
     vars: ref(composeMergeTags(props.variables)),
     config: ref({
       block_formats: "Paragraph=p; Header 1=h1; Header 2=h2; Header 3=h3; Header 4=h4",
-      plugins: "spellchecker mergetags anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount pagebreak checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker permanentpen powerpaste advtable advcode editimage tableofcontents footnotes mergetags autocorrect",
+      plugins: "mergetags anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount pagebreak checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker permanentpen powerpaste advtable advcode editimage tableofcontents footnotes mergetags autocorrect",
+      spellchecker_active: true,
       toolbar: "styles | bold italic underline strikethrough | link image align | numlist bullist indent outdent | removeformat",
       mergetags_list: composeMergeTags(props.variables),
       font_size_formats: "8pt 9pt 10pt 11pt 12pt 14pt 16pt 18pt 24pt 36pt 48pt",
@@ -83,7 +84,7 @@
   watch(() => props.variables, variablesNew => {
     const isIncludeMergeTags = variablesNew && variablesNew.length > 0;
 
-    let plugins = "anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount pagebreak checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker powerpaste advtable advcode editimage tableofcontents footnotes autocorrect autoresize";
+    let plugins = "tinymcespellchecker anchor autolink charmap code codesample emoticons image link lists media searchreplace table visualblocks wordcount pagebreak checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker powerpaste advtable advcode editimage tableofcontents footnotes autocorrect autoresize";
     if (isIncludeMergeTags) {
       plugins += " mergetags";
     }
