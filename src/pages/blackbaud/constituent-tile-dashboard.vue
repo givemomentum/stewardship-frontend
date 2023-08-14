03c1e58aa59b18b6d3810296dddd66068782db50
Markus Amalthea Magnuson <markus@polyscopic.works>
2023-08-14 03:03:41 +0200

    More auth handling in tile.

--- src/pages/blackbaud/constituent-tile-dashboard.vue
+++ src/pages/blackbaud/constituent-tile-dashboard.vue
@@ -7,6 +7,9 @@ import { useUserStore } from "~/apps/auth/useUserStore";
 
 const state = {
   userId: ref(""),
+  donorId: ref(""),
+  error: ref(""),
+  recommendations: ref([]),
 };
 
 const hooks = {
@@ -20,19 +23,31 @@ onBeforeMount(async () => {
   hooks.layout.activateLeanMode();
 });
 
-async function authenticate () {
+async function getUserId () {
   try {
     const token = await client.getUserIdentityToken();
     const res = await hooks.api.post("/crms/blackbaud/get-user-id", {
       token: token
     });
     if (res.status === 200) {
-      state.userId.value = res.data.user_id;
+      return res.data.user_id;
     }
   } catch (error) {
-    if (error.response?.status !== 403) {
-      throw error;
+    state.error.value = error.response.data.message;
+  }
+};
+
+async function getRecommendations (userId, donorId) {
+  try {
+    const res = await hooks.api.post("/crms/blackbaud/get-recommendations", {
+      user_id: userId,
+      donor_id: donorId,
+    });
+    if (res.status === 200) {
+      return res.data;
     }
+  } catch (error) {
+    state.error.value = error.response.data.message;
   }
 };
 
@@ -44,13 +59,27 @@ const client = new BBSkyAddinClient.AddinClient({
         title: 'Momentum'
       });
 
-      authenticate();
+      const userId = await getUserId();
+      state.userId.value = userId;
+
+      const donorId = args.context.recordId;
+      state.donorId.value = donorId;
+
+      const recommendations = await getRecommendations(userId, donorId);
+      state.recommendations.value = recommendations;
     }
   }
 });
 </script>
 
 <template>
-  <span v-if="!state.userId.value">Authenticating…</span>
-  <span v-if="state.userId.value">Blackbaud User ID: {{ state.userId.value }}</span>
+  <CFlex direction="column">
+    <div v-if="!state.userId.value">Authenticating…</div>
+    <div v-if="state.userId.value">Blackbaud User ID: {{ state.userId.value }}</div>
+    <div v-if="state.userId.value && state.donorId.value">Blackbaud Donor ID: {{ state.donorId.value }}</div>
+    <div style="color: red;" v-if="state.error.value">Error: {{ state.error.value }}</div>
+    <ul v-if="state.recommendations.value">
+      <li v-for="rec in state.recommendations.value">{{ rec }}</li>
+    </ul>
+  </CFlex>
 </template>
