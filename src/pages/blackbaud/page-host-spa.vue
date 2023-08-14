81d8b6813640e81c7a0b7eeae8a9ad55001bfd74
Markus Amalthea Magnuson <markus@polyscopic.works>
2023-08-14 03:03:30 +0200

    Simplify unfinished pages for now.

--- src/pages/blackbaud/page-host-spa.vue
+++ src/pages/blackbaud/page-host-spa.vue
@@ -1,41 +1,6 @@
 <script setup lang="ts">
 import * as BBSkyAddinClient from "@blackbaud/sky-addin-client";
 
-import { useApi } from "~/composables/useApi";
-import { useRoute } from "#app";
-import { useUserStore } from "~/apps/auth/useUserStore";
-
-const state = {
-  userId: ref(""),
-};
-
-const hooks = {
-  api: useApi(),
-  layout: useLayoutControl(),
-  route: useRoute(),
-  userStore: useUserStore(),
-};
-
-onBeforeMount(async () => {
-  hooks.layout.activateLeanMode();
-});
-
-async function authenticate () {
-  try {
-    const token = await client.getUserIdentityToken();
-    const res = await hooks.api.post("/crms/blackbaud/get-user-id", {
-      token: token
-    });
-    if (res.status === 200) {
-      state.userId.value = res.data.user_id;
-    }
-  } catch (error) {
-    if (error.response?.status !== 403) {
-      throw error;
-    }
-  }
-};
-
 const client = new BBSkyAddinClient.AddinClient({
   callbacks: {
     init: async function (args) {
@@ -43,14 +8,11 @@ const client = new BBSkyAddinClient.AddinClient({
         showUI: true,
         title: 'Momentum'
       });
-
-      authenticate();
     }
   }
 });
 </script>
 
 <template>
-  <span v-if="!state.userId.value">Authenticating…</span>
-  <span v-if="state.userId.value">Blackbaud User ID: {{ state.userId.value }}</span>
+  Hello SPA!
 </template>
