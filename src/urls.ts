95a77bd03d4d0ff17bc2b508d342db2c8384733d
Victor <36416236+victor-yunenko@users.noreply.github.com>
2023-11-03 18:47:39 -0700

    urls.ts: cleanup unused

--- src/urls.ts
+++ src/urls.ts
@@ -1,15 +1,6 @@
 import { PrimaryKey } from "~/apps/auth/interfaces";
 
 export namespace urls {
-  export const isCurrentPage = (url: string) => window.location.pathname === url;
-
-  export const tasks = {
-    list: "/tasks",
-    listAll: "/tasks/all",
-    detail: (slug: string) => `/tasks/${slug}`,
-    detailRec: (taskSlug: string, recSlug: string) => `/tasks/${taskSlug}/recs/${recSlug}`,
-  };
-
   export const portfolios = {
     portfolios: "/portfolios/",
     portfolio: (id: PrimaryKey) => `/portfolios/portfolio/${id}`,
@@ -22,44 +13,11 @@ export namespace urls {
     },
   };
 
-  export const fiscExport = {
-    list: "/fisc-exports",
-    detail: (id: number | string) => `/fisc-exports/${id}/`,
-    detailScans: (id: number, date: string) => `/fisc-exports/${id}/scans/${date}/`,
-  };
-
-  export const partners = {
-    hor: {
-      donationList: "/partners/hor/donation-list",
-    },
-  };
-
   export const emails = {
     base: "/emails/",
     batches: {
       list: "/emails/batches",
       edit: (batchPk: number) => `/emails/batch-${batchPk}/edit`,
-    },
-  };
-
-  export const letters = {
-    segmentList: "/letters/segments",
-    archive: "/letters/archive",
-    batchLettersList: (batchPk: number) => `/letters/segments/batches/${batchPk}/letters`,
-  };
-
-  export const organization = "/organization";
-  export const account = "/account";
-
-  export const donorPerfect = {
-    donor: (id: number | string) => `https://www.donorperfect.net/prod/ScreenDesigner/Donor/Edit/${id}`,
-  };
-
-  export const blackbaud = {
-    addins: {
-      constituentPageAction: "/blackbaud/constituent-page-action/",
-      constituentTileDashboard: "/blackbaud/constituent-tile-dashboard/",
-      pageHostSPA: "/blackbaud/page-host-spa/",
     },
   };
 }
